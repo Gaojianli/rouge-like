fd48abb785384752543c1772b5fa63aacc2ba36e
Gaojianli <823805669hehe@gmail.com>
2019-10-05 16:52:06 +0800

    select role

--- src/game.cpp
+++ src/game.cpp
@@ -9,12 +9,79 @@ void Game::init()
 
 void Game::start()
 {
-	mvprintw(LINES / 2, COLS / 2 - 27, "Please input your name:");
+	attrset(A_REVERSE);
+	for (int i = 0; i < COLS; ++i)
+		mvaddch(0, i, ' ');
+	mvprintw(0, COLS / 2 - 6, "Rouge-like");
+	attrset(A_NORMAL);
+	mvprintw(LINES / 2 - 2, COLS / 2 - 27, "Please input your name:");
 	refresh();
 	curs_set(1);//show the curs
 	echo();
 	auto name = new char[20];
 	flushinp();//flush the input
 	getnstr(name, 20);
+	move(LINES / 2 - 2, 0);
+	clrtoeol();
+	noecho();
 	player = make_shared<Player>(Player({ 0,0 }, name, Role::wizard));
+	mvprintw(4, COLS / 2 - 27, "Please select a role:");
+	const string roles[] = {
+		"magician",
+		"wizard",
+		"warrior",
+		"harpy",
+		"amazon",
+		"dwarf",
+		"monkey"
+	};
+	for (int i = 0; i < 7; i++) {
+		if (i == 0) {
+			string toInsert = "*";
+			toInsert += roles[i];
+			mvprintw(5 + i, COLS / 2 - 21, toInsert.c_str());//default select
+		}
+		else
+			mvprintw(5 + i, COLS / 2 - 20, roles[i].c_str());
+	}
+	int ch;
+	int postion = 0;
+	keypad(stdscr, TRUE);
+	bool selected = false;
+	curs_set(0);
+	while (!selected) {
+		ch = getch();
+		string toInsert = "*";
+		switch (ch)
+		{
+		case KEY_UP:
+			move(5 + postion, 0);
+			clrtoeol();
+			mvprintw(5 + postion, COLS / 2 - 20, roles[postion].c_str());
+			if (postion != 0)
+				postion--;
+			toInsert += roles[postion];
+			mvprintw(5 + postion, COLS / 2 - 21, toInsert.c_str());
+			refresh();
+			break;
+		case KEY_DOWN:
+			move(5 + postion, 0);
+			clrtoeol();
+			mvprintw(5 + postion, COLS / 2 - 20, roles[postion].c_str());
+			if (postion != 6)
+				postion++;
+			toInsert += roles[postion];
+			mvprintw(5 + postion, COLS / 2 - 21, toInsert.c_str());
+			refresh();
+			break;
+		case 13://enter
+			selected = true;
+			break;
+		default:
+			break;
+		}
+	}
+	refresh();
+	delete[] name;
+
 }
